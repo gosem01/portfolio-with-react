ade869fb066ccb1ac969c6ea2cb4d3ce317357c9
George S <reneehe@George-Sem-2.local>
2023-05-04 22:10:38 -0700

    move header links into navigation per requirements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import Header from './components/Header';
 import Contact from './components/Contact';
 import './App.css';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <><div>
+    <>
       <Header />
-    </div><div>
-        <Contact />
-      </div></>
+      <Navigation />
+      <Contact />
+
+    </>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,6 @@ const Header = () => {
     <header>
       <img src="logo.png" alt="Logo" />
       <h1>George S</h1>
-      <nav>
-        <ul>
-          <a href="#about">About me</a>
-          <a href="#portfolio">Portfolio</a>
-          <a href="#contact">Contact</a>
-          <a href="#resume">Resume</a>
-        </ul>
-      </nav>
     </header>
   );
 };
