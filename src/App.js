8bdff2fbeb5f02c749a4d323ab113b351d6ade15
George S <reneehe@George-Sem-2.local>
2023-05-07 18:06:14 -0700

    update app.js to correctly show pages, place nav links inside header and add some styling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Contact from './components/Contact';
 import './App.css';
@@ -7,60 +8,69 @@ import Project from './components/Project';
 
 function App() {
   const [showProjects, setShowProjects] = useState(false);
+  const [showContact, setShowContact] = useState(false);
+  const [showAbout, setShowAbout] = useState(false);
+  
 
   const projects = [
     {
-      title: 'Turn Up',
-      description: 'Find local events and meetups near you.',
-      imageUrl: 'logo192.png',
+      title: 'Project 1',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      imageUrl: 'project1.jpg',
       projectUrl: 'https://example.com/project1',
     },
     {
-      title: 'National Park Reviewer 5000',
-      description: 'Write and read about national parks. Find out if they are worth visiting.',
-      imageUrl: 'logo192.png',
+      title: 'Project 2',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      imageUrl: 'project2.jpg',
       projectUrl: 'https://example.com/project2',
     },
   ];
 
-  useEffect(() => {
-    if (window.location.hash === '#portfolio') {
-      setShowProjects(true);
-    }
 
-    window.onhashchange = () => {
-      if (window.location.hash === '#portfolio') {
-        setShowProjects(true);
-      } else {
-        setShowProjects(false);
-      }
+  useEffect(() => {
+    const handleHashChange = () => {
+      const hash = window.location.hash;
+      setShowAbout(hash === '#about');
+      setShowProjects(hash === '#portfolio');
+      setShowContact(hash === '#contact');
     };
+
+    window.addEventListener('hashchange', handleHashChange);
+    handleHashChange();
+    return () => window.removeEventListener('hashchange', handleHashChange);
   }, []);
 
-  if (!showProjects) {
-    return null;
-  }
-
   return (
-    <>
+    <div className="App">
       <Header />
-      <Navigation />
-      <section id="portfolio">
-        <h2>Portfolio</h2>
-        {projects.map((project, index) => (
-          <Project
-            key={index}
-            title={project.title}
-            description={project.description}
-            imageUrl={project.imageUrl}
-            projectUrl={project.projectUrl}
-          />
-        ))}
-      </section>
-      <Contact />
+      {/* <Navigation /> */}
+      {showProjects && (
+        <section id="portfolio">
+          <h2>Portfolio</h2>
+          {projects.map((project, index) => (
+            <Project
+              key={index}
+              title={project.title}
+              description={project.description}
+              imageUrl={project.imageUrl}
+              projectUrl={project.projectUrl}
+            />
+          ))}
+        </section>
+      )}
+      {showContact && <Contact />}
+      {showAbout && (
+        <section id="about">
+          <h2>About Me</h2>
+          <p>My name is George and I am an aspiring software engineer who is ready to tackle any problem.</p>
+          <p>I love working with code and creating software out of thin air. To me this is the most magical thing.</p>
+          <p>Welcome to my website and feel free to browse around and contact me if you interested in connecting!</p>
+        </section>
+      )}
       <Footer />
-    </>
+    </div>
   );
-}
+};
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navigation from './Navigation';
 
 const Header = () => {
   return (
     <header>
       <img src="logo.png" alt="Logo" />
       <h1>George S</h1>
+      <Navigation />
     </header>
   );
 };
